7bfa1d5f94222a5698ed8385f8e9d85c002f4226
Rayyan Shaikh <95620419+XRegiGigaSX@users.noreply.github.com>
2023-04-17 20:27:07 +0530

    Update index.js
    UPDATE: base url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: 'http://localhost:3001'})
+const API = axios.create({baseURL: 'https://polaroid-api.vercel.app/'})
 // const url = 'http://localhost:3001/posts';
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
